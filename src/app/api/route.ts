d4d60ab3c29e129608e90be7abfab67d75b12acb
Nabiur siddique <nabiursiddique01@gmail.com>
2024-09-04 22:31:26 +0600

    basic api route in nextjs is implemented

---
+++ src/app/api/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  return NextResponse.json({ message: 'Hello From Server' });
+};
+
+export const POST = async (req: Request) => {
+  return NextResponse.json({ message: 'Hello From Server' });
+};
+
+export const DELETE = async () => {
+  return NextResponse.json({ message: 'Hello From Server' });
+};
+
+export const PUT = async () => {
+  return NextResponse.json({ message: 'Hello From Server' });
+};
+
+export const PATCH = async () => {
+  return NextResponse.json({ message: 'Hello From Server' });
+};
